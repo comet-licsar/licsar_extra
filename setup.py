9ea00460b058ad162dcb3b7b15e1d841b0113d53
laz048 <milan.lazecky@vsb.cz>
2023-08-31 14:08:02 +0100

    to have it pip-installable

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='licsar_extra',
+    version='0.1',
+    description='LiCSAR Extra set of tools',
+    url='https://github.com/comet-licsar/licsar_extra',
+    author='Milan Lazecky',
+    author_email='M.Lazecky@leeds.ac.uk',
+    license='GNU-GPL3.0',
+    packages=['licsar_extra'],
+    package_dir = {'licsar_extra':'python'}
+    install_requires=['numpy<2.0',
+'os','glob','shutil','subprocess','re','time',
+'matplotlib','xarray','rioxarray','pandas',
+'scipy','astropy','scikit-learn','gdal>=2.4']
+)
